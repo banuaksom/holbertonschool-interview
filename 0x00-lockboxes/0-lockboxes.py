fbbd65cd750c1a5c9b9d3fedb19f5b67e6d4fe53
Banu <banu@Sergeys-MacBook-Pro.local>
2020-07-18 14:44:20 -0700

    added extra checks

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,15 +3,17 @@
 
 def canUnlockAll(boxes):
     open = [False for i in range(len(boxes))]
+    if len(boxes) == 0:
+        return True
     open[0] = True
-    l1 = [x for x in boxes[0]]
+    l1 = [0]
     while len(l1) > 0:
         l2 = []
         for i in l1:
-            open[i] = True
             for num in boxes[i]:
-                if not open[num]:
+                if num < len(boxes) and not open[num]:
                     l2.append(num)
+                    open[num] = True
         l1 = l2
     for i in open:
         if not i:
