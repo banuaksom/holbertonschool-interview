8e72b936d95daf30b2f1d5023c2cccb60f41b006
Banu <banu@Sergeys-MacBook-Pro.local>
2020-07-18 19:03:40 -0700

    edited return syntax

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,6 +7,7 @@ def canUnlockAll(boxes):
         return True
     open[0] = True
     l1 = [0]
+
     while len(l1) > 0:
         l2 = []
         for i in l1:
@@ -15,8 +16,5 @@ def canUnlockAll(boxes):
                     l2.append(num)
                     open[num] = True
         l1 = l2
-    for i in open:
-        if not i:
-            return False
 
-    return True
+    return all(open)
