8972acf516eb4177f6e9e39caf462c04113eb2f6
Banu Sapakova <banuaksom@gmail.com>
2020-08-18 00:37:41 +0000

    betty issues

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,100 +9,106 @@
  */
 void grid_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
 }
 
 /**
  * print_grid - prints the grid
  * @grid: grid
  */
-
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
-
 /**
-* sandpiles_sum - computes the sum of two matrixes
-* @grid1: matrix1
-* @grid2: matrix2
-*/
+ * refill_grid - rewrites grid with 0
+ * @grid: grid to rewrite
+ */
+void refill_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid[i][j] = 0;
+	}
+}
+/**
+ * overflow - spreads to sides if num > 3
+ * @grid1: grid 1
+ * @grid2: grid 2
+ */
+void overflow(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid2[i][j] == 1)
+			{
+				grid1[i][j] -= 4;
+				if (j + 1 < 3)
+					grid1[i][j + 1] += 1;
+				if (j - 1 >= 0)
+					grid1[i][j - 1] += 1;
+				if (i + 1 < 3)
+					grid1[i + 1][j] += 1;
+				if (i - 1 >= 0)
+					grid1[i - 1][j] += 1;
+			}
+		}
+	}
+}
+/**
+ * sandpiles_sum - computes the sum of two matrixes
+ * @grid1: matrix1
+ * @grid2: matrix2
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j, flag;
+	int i, j, flag;
 
-    grid_sum(grid1, grid2);
-
-    while (1)
-    {
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-                grid2[i][j] = 0;
-        }
-
-        flag = 0;
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid1[i][j] > 3)
-                {
-                    grid2[i][j] = 1;
-                    flag = 1;
-                }
-            }
-        }
-        if (flag)
-        {
-            printf("=\n");
-            print_grid(grid1);
-        }
-        else if (flag == 0)
-        {
-            break;
-        }
-
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid2[i][j] == 1)
-                {
-                    grid1[i][j] -= 4;
-
-                    if (j + 1 < 3)
-                        grid1[i][j + 1] += 1;
-
-                    if (j - 1 >= 0)
-                        grid1[i][j - 1] += 1;
-
-                    if (i + 1 < 3)
-                        grid1[i + 1][j] += 1;
-
-                    if (i - 1 >= 0)
-                        grid1[i - 1][j] += 1;
-                }
-            }
-        }
-    }
+	grid_sum(grid1, grid2);
+	while (1)
+	{
+		refill_grid(grid2);
+		flag = 0;
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					grid2[i][j] = 1;
+					flag = 1;
+				}
+			}
+		}
+		if (flag)
+		{
+			printf("=\n");
+			print_grid(grid1);
+		}
+		else if (flag == 0)
+			break;
+		overflow(grid1, grid2);
+	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -3,4 +3,6 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void grid_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
+void refill_grid(int grid[3][3]);
+void overflow(int grid1[3][3], int grid2[3][3]);
 #endif
