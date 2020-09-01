36eb7c8cacbf57ee8aa0a55dd069b36fa81a7909
Banu Sapakova <banuaksom@gmail.com>
2020-09-01 18:24:51 +0000

    pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,18 +3,17 @@
 
 if __name__ == '__main__':
     import sys
-    
+
     fileSize, counter = 0, 0
-    
+
     stats = {"200": 0, "301": 0, "400": 0, "401": 0,
-         "403": 0, "404": 0, "405": 0, "500": 0}
-         
+             "403": 0, "404": 0, "405": 0, "500": 0}
+
     def report(stats, fileSize):
         print("File size: {:d}".format(fileSize))
         for i in sorted(stats.keys()):
             if stats[i] != 0:
                 print("{}: {:d}".format(i, stats[i]))
-
 
     try:
         for line in sys.stdin:
@@ -22,12 +21,10 @@ if __name__ == '__main__':
                 report(stats, fileSize)
             strline = line.split()
             counter += 1
-            
             try:
                 fileSize += int(strline[-1])
             except:
                 pass
-            
             try:
                 if strline[-2] in stats:
                     stats[strline[-2]] += 1
