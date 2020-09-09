d67410f523c65a7ce44a749f45139bf2a5f1df3d
Banu <banu@Sergeys-MacBook-Pro.local>
2020-09-09 10:55:13 -0700

    first commit

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: - pointer
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow_p = list, *fast_p = list;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+			return (1);
+	}
+	return (0);
+}
