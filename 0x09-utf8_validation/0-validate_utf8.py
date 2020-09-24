697d83e55a047f776fbbc599e919571af3d4137c
Banu <banu@Sergeys-MacBook-Pro.local>
2020-09-23 19:40:07 -0700

    added

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,19 +8,10 @@ def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding
     """
-    num_bytes = 0
-    for n in data:
-        mask = 1 << 7
-        if not num_bytes:
-            while mask & n:
-                num_bytes += 1
-                mask >>= 1
-            if not num_bytes:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
-                return False
+    valid = []
+    for i in data:
+        if (i >= 0 and i < 256):
+            valid.append(True)
         else:
-            if n >> 6 != 0b10:
-                return False
-        num_bytes -= 1
-    return num_bytes == 0
+            valid.append(False)
+    return (all(valid))
