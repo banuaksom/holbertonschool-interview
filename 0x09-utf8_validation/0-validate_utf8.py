785a73ce27a78ed831a8ccf4e63dcf78011fc705
Banu <banu@Sergeys-MacBook-Pro.local>
2020-09-23 19:32:00 -0700

    added

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -2,10 +2,19 @@
 
 
 def validUTF8(data):
-    valid = []
-    for i in data:
-        if (i >= 0 and i < 256):
-            valid.append(True)
+    num_bytes = 0
+    for n in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while mask & n:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
         else:
-            valid.append(False)
-    return all(valid)
+            if n >> 6 != 0b10:
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
